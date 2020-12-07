f4f3a9865fc8cb622d55cefa3c73e3a7839be951
ptkk73 <ptkk73@gmail.com>
2020-12-07 14:22:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mkymsa-zhe5sj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mkymsa-zhe5sj)
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartService } from "./cart.service";
+import { CartComponent } from "./cart/cart.component";
+
+import { HttpClientModule } from "@angular/common/http";
+import { ShippingComponentComponent } from './shipping-component/shipping-component.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent },
+      { path: "shipping", component: ShippingComponentComponent}
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponentComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [CartService]
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
